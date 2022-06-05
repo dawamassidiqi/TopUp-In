ca74f5c2b19ccd4f42a1c1bdf282fd86c4db5ced
meliusa <meliusanorahariyanti@gmail.com>
2022-06-05 22:57:48 +0800

    Meliusa - Migration and Models

--- database/migrations/2022_06_01_211712_create_credits_table.php
+++ database/migrations/2022_06_01_211712_create_credits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCreditsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('credits', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('photo')->nullable();
-            $table->rememberToken();
+            $table->foreignId('game_id')->constrained();
+            
+            $table->double('price');
+            $table->text('information');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('credits');
     }
 }
